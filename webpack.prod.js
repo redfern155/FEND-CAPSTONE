9fca743c45bfbdd28cc5a8139e27127490220166
redfern155 <66224171+redfern155@users.noreply.github.com>
2020-09-08 21:07:04 -0400

    Added service workers

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const HTMLWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -42,6 +43,7 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css'
-        })
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
